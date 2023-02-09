62607057a55d7dcd5a8ed148e53efe328647d42c
Handol Park <handolpark12@gmail.com>
2023-02-08 17:35:10 -0800

    nix flake init attention-is-all-you-need

---
+++ attention-is-all-you-need/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        myApp = mkPoetryApplication {
+          projectDir = self;
+        };
+      in
+      {
+        packages = {
+          myApp = myApp;
+          default = self.packages.${system}.myApp;
+        };
+      });
+}
